d5511583989df5f7393fd772d9fc58aecc2a9f5d
FlorianCohenJoly <floriancohenjoy@gmail.com>
2023-10-20 18:23:34 +0200

    Add new position with an camera

--- Overworld.js
+++ Overworld.js
@@ -12,20 +12,27 @@ class Overworld {
             // Clear the canvas
             this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
-            // Draw Lower layer
-            this.maps.drawLowerImage(this.ctx);
+            // Establish the camera position
+            const cameraPerson = this.maps.gameObjects.hero;
 
-            // Draw Game Objects
             Object.values(this.maps.gameObjects).forEach(object => {
                 object.update({
                     arrow: this.directionInput.direction,
 
                 });
-                object.sprite.draw(this.ctx);
+            });
+
+            // Draw Lower layer
+            this.maps.drawLowerImage(this.ctx, cameraPerson);
+
+            // Draw Game Objects
+            Object.values(this.maps.gameObjects).forEach(object => {
+
+                object.sprite.draw(this.ctx, cameraPerson);
             });
 
             // Draw Upper layer
-            this.maps.drawUpperImage(this.ctx);
+            this.maps.drawUpperImage(this.ctx, cameraPerson);
 
             requestAnimationFrame(() => {
                 step();
